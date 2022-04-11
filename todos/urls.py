2b5e8829be2d4d2c3d166d1fbf05a3f61bc62828
whoamins <40am1ns@protonmail.com>
2022-04-11 22:46:40 +0300

    Class-based view

--- todos/urls.py
+++ todos/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from todos import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('update/<int:task_id>/', views.update_status, name='update_status')
+    path('', views.IndexView.as_view(), name='index'),
+    path('delete/<int:task_id>/', views.DeleteView.as_view(), name='delete_task'),
+    path('update/<int:task_id>/', views.UpdateView.as_view(), name='update_status')
 ]
 
--- todos/views.py
+++ todos/views.py
@@ -1,14 +1,19 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import HttpResponse, render, redirect
+from django.views import View
 
 from todos.forms import TaskForm
 from todos.models import Task
 
 
-def index(request: WSGIRequest) -> HttpResponse:
-    form = TaskForm()
+class IndexView(View):
+    def get(self, request):
+        form = TaskForm()
+        tasks = Task.objects.all()
+        return render(request, "todos/index.html", {"task_form": form, "tasks": tasks})
 
-    if request.method == "POST":
+
+    def post(self, request):
         form = TaskForm(request.POST)
 
         if form.is_valid():
@@ -16,21 +21,19 @@ def index(request: WSGIRequest) -> HttpResponse:
 
             return redirect("index")
 
-    tasks = Task.objects.all()
 
-    return render(request, "todos/index.html", {"task_form": form, "tasks": tasks})
+class DeleteView(View):
+    def get(request: WSGIRequest, task_id: int) -> HttpResponse:  # Maybe post?
+        task = Task.objects.get(pk=task_id)
+        task.delete()
 
-
-def delete_task(request: WSGIRequest, task_id: int) -> HttpResponse:
-    task = Task.objects.get(pk=task_id)
-    task.delete()
-
-    return redirect("index")
+        return redirect("index")
 
 
-def update_status(request: WSGIRequest, task_id: int) -> HttpResponse:
-    task = Task.objects.get(pk=task_id)
-    task.completed = True
-    task.save()
+class UpdateView(View):
+    def GET(request: WSGIRequest, task_id: int) -> HttpResponse:  # Maybe post?
+        task = Task.objects.get(pk=task_id)
+        task.completed = True
+        task.save()
 
-    return redirect("index")
+        return redirect("index")
